b504bef594cdd0d9a6c779d8a0bfb097725531a0
LuisSerafini <serafinilf@gmail.com>
2023-10-23 20:35:05 -0300

    subindo

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\PessoaController;
+use App\Http\Controllers\api\PortariaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,12 +16,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//chamada da rota http://localhost:800/api/ ->
+
+//rodar o php artisan route:list
+
+
+/**
+ * GET|HEAD  api/pessoa ................................................................................................................................ api\PessoaController@index  
+ * POST      api/pessoa ................................................................................................................................ api\PessoaController@store  
+ * GET|HEAD  api/pessoa/proc/{nome} ..................................................................................................................... api\PessoaController@show  
+ * DELETE    api/pessoa/{id} ......................................................................................................................... api\PessoaController@destroy  
+ * PUT       api/pessoa/{id} .......................................................................................................................... api\PessoaController@update 
+ */
+
 Route::get('/pessoa', [PessoaController::class, 'index']);                      //ok
 Route::delete('/pessoa/{id}', [PessoaController::class, 'destroy']);            //ok
 Route::post('/pessoa',[PessoaController::class, 'store']);                      //ok
 Route::get('/pessoa/proc/{nome}', [PessoaController::class, 'show']);           //ok
 Route::put('/pessoa/{id}',[PessoaController::class, 'update']);                 //ok
-//Route::get('/pessoa/edit/{id}',[PessoaController::class, 'edit']);              //-falta
+//Route::get('/pessoa/edit/{id}',[PessoaController::class, 'edit']);            //-falta
+
+//Portaria rotas
+//chamada da rota http://localhost:800/api/ ->
+Route::get('/portaria', [PortariaController::class, 'index']);
+Route::delete('/portaria/{id}', [PortariaController::class, 'destroy']);
+Route::post('/portaria', [PortariaController::class, 'store']);
+Route::get('/portaria/proc/{nome}', [PortariaController::class, 'show']);
+Route::put('/portaria/{id}', [PortariaController::class, 'update']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
